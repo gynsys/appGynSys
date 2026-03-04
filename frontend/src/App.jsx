71395774ffdfcbaf930c6f30fb58f0f270bd27cb
gynsys <dramarielh@gmail.com>
2026-03-04 08:53:55 -0400

    Clean up redundant PricingPage and unify SaaS landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { Toaster } from 'sonner'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store/authStore'
 import LandingPage from './pages/LandingPage'
-import PricingPage from './pages/PricingPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
@@ -137,7 +136,6 @@ function App() {
             <LandingPage />
           )
         } />
-        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
