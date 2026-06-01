3626bb68b2c89cafd03c3f025d9fbd58cb3c176a
gynsys <dramarielh@gmail.com>
2026-05-31 22:32:28 -0400

    chore: add dummy

---
+++ backend/alembic/versions/60c8b1c4413d_dummy.py
@@ -0,0 +1,17 @@
+"""dummy
+
+Revision ID: 60c8b1c4413d
+Revises: 0b11fb8ec236
+Create Date: 2026-06-01
+
+"""
+from alembic import op
+import sqlalchemy as sa
+revision = "60c8b1c4413d"
+down_revision = "0b11fb8ec236"
+branch_labels = None
+depends_on = None
+def upgrade() -> None:
+    pass
+def downgrade() -> None:
+    pass
